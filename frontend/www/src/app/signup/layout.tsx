73192f6069cc857d9707070486ecda9304339f42
Allen Lin <allenlinsh@gmail.com>
2024-10-27 20:20:46 -0400

    add auth metadata

---
+++ frontend/www/src/app/signup/layout.tsx
@@ -0,0 +1,19 @@
+import { defaultMetadata, defaultOpenGraph } from "@/constants/metadata";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  ...defaultMetadata,
+  title: "Sign Up | Textfully",
+  openGraph: {
+    ...defaultOpenGraph,
+    title: "Sign Up | Textfully",
+  },
+};
+
+export default function TermsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
